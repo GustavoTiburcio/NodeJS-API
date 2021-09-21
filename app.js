dae788de84aeff2e59b31e63882f81b359e5eef2
GustavoTiburcio <gustavotiburcio23@gmail.com>
2021-09-20 22:12:50 -0300

    configurações de Header e CORS

--- app.js
+++ app.js
@@ -1,10 +1,46 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const rotaProdutos = require('./routes/produtos')
 const rotaPedidos = require('./routes/pedidos')
 
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false })); //Apenas dados simples
+app.use(express.json()); //Somente JSON no body
+
+app.use((req, res, next) => {
+    res.header('Acess-Control-Allow-Origin', '*')
+    res.header(
+        'Acess-Control-Allow-Header',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+
+    if (req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+        return res.status(200).send({});
+    }
+
+    next();
+});
+
 app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
+
+//Quando não encontra rota
+app.use((req, res, next) => {
+    const erro = new Error('Não encontrado');
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            mensagem: error.message 
+        }
+    })
+});
 
 module.exports = app;
--- routes/produtos.js
+++ routes/produtos.js
@@ -10,8 +10,13 @@ router.get('/', (req, res, next) =>{
 
 //INSERE UM PRODUTOS
 router.post('/', (req, res, next) => {
+    const produto = {
+        nome: req.body.nome,
+        preco: req.body.preco
+    }
     res.status(201).send({
-        mensagem: 'Insere um produto'
+        mensagem: 'Insere um produto',
+        produtoCriado: produto
     });
 });
 

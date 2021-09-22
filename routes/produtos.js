f319d6168899be38260dcd90d0f452a91a9d3339
GustavoTiburcio <gustavotiburcio23@gmail.com>
2021-09-22 18:08:11 -0300

    Melhorando response dos metodos

--- routes/produtos.js
+++ routes/produtos.js
@@ -4,83 +4,116 @@ const postgres = require('../postgres');
 
 // RETORNA TODOS OS PRODUTOS
 router.get('/', (req, res, next) =>{
-    postgres.query('SELECT * from produtos', (error, resp) => {
+    postgres.query('SELECT * FROM produtos ORDER BY id', (error, result) => {
         if (error) {
             return res.status(500).send({
-                error: error,
-                response: null
+                error: error
             });
         }
-        console.log(resp)
-        const response = resp.rows;
-        res.status(200).send({
-                 response
-             });
+        console.log(result)
+        const response = {
+            totalDeRegistros: result.rowCount,
+            produtos: result.rows.map(prod => {
+                return {
+                    id_produto: prod.id,
+                    nome: prod.nome,
+                    preco: prod.preco,
+                    request: {
+                        tipo: 'GET',
+                        descricao: 'Retorna todos os produtos',
+                        url: 'http://localhost:3000/produtos/' + prod.id
+                    }
+                }
+            })
+        }
+        res.status(200).send(response);
       })
 });
 
 //INSERE UM PRODUTO
 router.post('/', (req, res, next) => {
-    postgres.query('INSERT INTO produtos (nome, preco) VALUES ($1, $2)',[req.body.nome, req.body.preco], (error, resp) => {
+    postgres.query('INSERT INTO produtos (nome, preco) VALUES ($1, $2) RETURNING id',[req.body.nome, req.body.preco], (error, result) => {
         if (error) {
             return res.status(500).send({
-                error: error,
-                response: null
+                error: error
             });
         }
-        console.log(resp)
-        res.status(201).send({
-                 mensagem: 'Produto inserido com sucesso'
-             });
+        console.log(result)
+        const response = {
+            mensagem: 'Produto inserido com sucesso',
+            produtoCriado:{
+                id_produto: result.rows[0].id,
+                nome: req.body.nome,
+                preco: req.body.preco,
+                request: {
+                    tipo: 'POST',
+                    descricao: 'Insere um produto',
+                    url: 'http://localhost:3000/produtos'
+                }
+            }
+        }
+        res.status(201).send(response);
       });
 });
 
 //RETORNA OS DADOS DE UM PRODUTO
 router.get('/:id_produto', (req, res, next) => {
     //const id = req.params.id_produto;
-    postgres.query('SELECT * from produtos where id = $1', [req.params.id_produto],(error, resp) => {
+    postgres.query('SELECT * FROM produtos WHERE id = $1', [req.params.id_produto],(error, result) => {
         if (error) {
             return res.status(500).send({
-                error: error,
-                response: null
+                error: error
             });
         }
-        console.log(resp)
-        const response = resp.rows;
-        res.status(200).send({
-                 response
-             });
+        console.log(result)
+        if (result.rowCount == 0) {
+            return res.status(404).send({
+                mensagem: 'Não foi encontrado produto com esse ID'
+            });
+        }
+        const response = {
+            produto:{
+                id_produto: result.rows[0].id,
+                nome: result.rows[0].nome,
+                preco: result.rows[0].preco,
+                request: {
+                    tipo: 'GET',
+                    descricao: 'Retorna um produto',
+                    url: 'http://localhost:3000/produtos'
+                }
+            }
+        }
+        const resultado = result.rows;
+        res.status(200).send(response);
       })
 });
 
 //ALTERA UM PRODUTO
-router.patch('/:id_produto', (req, res, next) => {
-    postgres.query('UPDATE produtos set nome = $2, preco = $3 where id = $1', [req.params.id_produto, req.body.nome, req.body.preco],(error, resp) => {
+router.patch('/', (req, res, next) => {
+    postgres.query('UPDATE produtos SET nome = $1, preco = $2 WHERE id = $3', [req.body.nome, req.body.preco, req.body.id_produto],(error, result) => {
         if (error) {
             return res.status(500).send({
-                error: error,
-                response: null
+                error: error
             });
         }
-        console.log(resp)
-        res.status(200).send({
-                mensagem: 'produto alterado'
+        console.log(result)
+        res.status(202).send({
+                mensagem: 'produto alterado com sucesso'
              });
       })
 });
 
 //EXCLUI UM PRODUTO
-router.delete('/:id_produto', (req, res, next) => {
-    postgres.query('DELETE from produtos where id = $1', [req.params.id_produto],(error, resp) => {
+router.delete('/', (req, res, next) => {
+    postgres.query('DELETE FROM produtos WHERE id = $1', [req.body.id_produto],(error, result) => {
         if (error) {
             return res.status(500).send({
-                error: error,
-                response: null
+                error: error
             });
         }
-        console.log(resp)
-        res.status(200).send({
-                mensagem: 'produto excluído'
+        console.log(result)
+        res.status(202).send({
+                mensagem: 'produto excluído com sucesso'
              });
       })
 });

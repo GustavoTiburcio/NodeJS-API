dae5d08749b9726650b42b1d8dfa6e8b0de7f39f
GustavoTiburcio <gustavotiburcio23@gmail.com>
2021-10-05 15:32:26 -0300

    heroku deploy

--- postgres.js
+++ postgres.js
@@ -1,10 +1,11 @@
-const {Pool} = require('pg')
-const pool = new Pool({
-  user: process.env.POSTGRES_USER,
-  host: process.env.POSTGRES_HOST,
-  database: process.env.POSTGRES_DATABASE,
-  password: process.env.POSTGRES_PASSWORD,
-  port: process.env.PORT
-});
+const { Pool } = require('pg');
+const { parse } = require('pg-connection-string')
+
+const config = parse(process.env.DATABASE_URL)
+
+config.ssl = {
+  rejectUnauthorized: false
+}
+const pool = new Pool(config)
 
 module.exports = pool;

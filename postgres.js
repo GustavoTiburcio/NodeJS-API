0d8909d9e19ab370d81f88ac26844fd4a549d945
GustavoTiburcio <gustavotiburcio23@gmail.com>
2021-10-05 15:37:22 -0300

    teste heroku

--- postgres.js
+++ postgres.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 const { parse } = require('pg-connection-string')
 
-const config = parse(process.env.DATABASE_URL)
+const config = parse('postgres://borgdgvuyheyvs:b652809a1d9d7668dabf0605e3dd6f600dda09b20b73955a70ba95ab5beafa0b@ec2-52-206-193-199.compute-1.amazonaws.com:5432/d8drtbbcondv11')
 
 config.ssl = {
   rejectUnauthorized: false
